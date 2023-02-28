431af37ac8bccf89086cdd494409fce9f8bad827
reymisterii <reymisterii@gmail.com>
2023-02-28 15:32:23 +0200

    newMentor

--- js/task-02.js
+++ js/task-02.js
@@ -9,35 +9,17 @@ const ingredients = [
 
 const ulEl = document.querySelector(`#ingredients`);
 
-// ingredients.forEach((ingredient) => {
+const newArray = [];
 
-// const titleEl = document.createElement(`li`);
-// titleEl.classList.add(`item`);
-// titleEl.textContent = ingredient;
+ingredients.forEach((ingredient) => {
+
+const titleEl = document.createElement(`li`);
+titleEl.classList.add(`item`);
+  titleEl.textContent = ingredient;
   
-// ulEl.append(titleEl);
-//   console.log(titleEl);
+  newArray.push(titleEl);
+  console.log(titleEl);
+});
+console.log(newArray);
 
-// });
-
-const firstTitleEl = document.createElement(`li`);
-firstTitleEl.classList.add(`item`);
-firstTitleEl.textContent = ingredients[0];
- const secondTitleEl = document.createElement(`li`);
- secondTitleEl.classList.add(`item`);
- secondTitleEl.textContent = ingredients[1];
-const thirdTitleEl = document.createElement(`li`);
-thirdTitleEl.classList.add(`item`);
-thirdTitleEl.textContent = ingredients[2];
-const fourthTitleEl = document.createElement(`li`);
-fourthTitleEl.classList.add(`item`);
-fourthTitleEl.textContent = ingredients[3];
-const fifthTitleEl = document.createElement(`li`);
-fifthTitleEl.classList.add(`item`);
-fifthTitleEl.textContent = ingredients[4];
-const sixtTitleEl = document.createElement(`li`);
-sixtTitleEl.classList.add(`item`);
-sixtTitleEl.textContent = ingredients[5];
-
- 
- ulEl.append(firstTitleEl, secondTitleEl, thirdTitleEl, fourthTitleEl, fifthTitleEl, sixtTitleEl);
+ulEl.append(...newArray);
--- js/task-08.js
+++ js/task-08.js
@@ -2,11 +2,11 @@ const formRef = document.querySelector(`.login-form`);
 
 const inputEmail = formRef.firstElementChild.firstElementChild;
 inputEmail.placeholder = 'Введіть ваше імя';
-console.log(inputEmail);
+
 
 const inputPassword = formRef.firstElementChild.nextElementSibling.firstElementChild;
 inputPassword.placeholder = '********';
-console.log(inputPassword);
+
 
 formRef.addEventListener(`submit`, (e) => {
    e.preventDefault();
@@ -18,20 +18,12 @@ formRef.addEventListener(`submit`, (e) => {
    if (mail === "" || password === "") {
       return alert(`Заповніть будь-ласка всі поля`);
    }
-   console.log(e);
-   console.log(`Login:${mail}, Password: ${password}`);
 
    const formData = {
       mail,
       password,
    }
    console.log(formData);
-});
-
-formRef.addEventListener(`submit`, resetInput);
-
-function resetInput () {
    formRef.reset();
-};
-
+});
 
